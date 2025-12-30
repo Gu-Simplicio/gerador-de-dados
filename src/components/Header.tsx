d50e018b7b0498c550a3b06c0fbde2f659ef4e72
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-12-29 22:19:11 -0300

    componente Header criado

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header>
+            <a href="/">
+                Gerador de dados.. 
+            </a>
+        </header>
+    )
+}
+
+export default Header;
