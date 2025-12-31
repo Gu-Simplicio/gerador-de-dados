841133915e0353da2eb9d7e4b8496edc4f5e81b6
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-12-31 01:20:35 -0300

    header começando a ser estilizado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,30 @@
+import HeaderLink from "./HeaderLink";
+
 function Header() {
     return (
-        <header>
+        <header className="
+            fixed 
+            top-0 
+            left-0 
+            right-0 
+            px-3
+            py-1
+            flex 
+            justify-between
+            shadow">
             <a href="/">
-                Gerador de dados.. 
+                Gerador de dados
             </a>
+
+            <ul className="
+                flex
+                gap-3">
+                <li>
+                    <HeaderLink 
+                        url="/"
+                        txt="Teste"/>
+                </li>
+            </ul>
         </header>
     )
 }
