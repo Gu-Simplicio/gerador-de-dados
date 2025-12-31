10cc21d4088b5fb6c270f64350aa39119470f5e0
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2025-12-31 02:17:22 -0300

    página principal recebendo e exibindo os cards salvos no JSON

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 import Header from './components/Header/Header'
+import type { CardData } from './interfaces/CardData';
 
 function App() {
+  // contém todos os cards salvos no JSON
+  const [cards, setCards] = useState<CardData[]>([]);
+
+  useEffect(() => { // carrega os cards do JSON
+    fetch('./cards.json', {
+      headers: {
+        Accept: "Application/json"
+      }
+    }).then( res => res.json() )
+      .then( res => setCards(res.data));
+  }, []);
+
   return (
     <>
       <Header />
@@ -10,14 +24,20 @@ function App() {
       <main className="
                   flex 
                   gap-5
+                  items-center
                   flex-col
-                  px-3
                   mt-10">
-        <Card 
-          title="Teste1"
-          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis vero odit nisi accusantium tempora molestiae atque consequuntur sapiente amet dolorum! Quos corrupti quae dicta maiores hic, quo modi ut placeat?"
-          btnUrl="/teste1"
-          btnTxt='Teste 01'/>
+        {
+          cards.map((card, index) => (
+            <Card 
+              title={card.title}  
+              text={card.text}
+              btnUrl={card.btnUrl}
+              btnTxt={card.btnTxt}
+              key={index}
+              />
+          ))
+        }
       </main>
     </>
   )
