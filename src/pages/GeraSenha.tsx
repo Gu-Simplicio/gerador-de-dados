36934d73f1a4f0b9cee766c27f8021f3a1432569
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-01-06 00:11:56 -0300

    conteúdo da página de gerar senha inicial inserido

--- src/pages/GeraSenha.tsx
+++ src/pages/GeraSenha.tsx
@@ -1,7 +1,42 @@
 import Header from "../components/Header/Header"
 
 function GeraSenha() {
-    return <Header />
+    return (
+        <>
+            <Header />
+        
+            <main className="
+                 flex 
+                  gap-5
+                  items-center
+                  flex-col
+                  mt-16">
+                <h1> Gerar senha </h1>
+
+                <button className="
+                            py-1
+                            px-2
+                            cursor-pointer
+                            text-white
+                            font-bold
+                            bg-red-500
+                            duration-300
+                            ease-in
+                            hover:rounded-full"
+                            onClick={() => { alert("Hello, world") }}>
+                    Gerar:  
+                </button>
+
+                <input 
+                    type="text" 
+                    id="inCNPJ" 
+                    placeholder="O CNPJ vai aparecer aqui.." 
+                    disabled={true}  
+                    value={"cnpjGerado"}
+                    className="border-1"/>
+            </main>
+        </>
+    )
 }
 
 export default GeraSenha
