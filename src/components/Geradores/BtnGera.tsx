79cc9fd6a7384da18637e93a57ba8287c1d6a8c5
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-01-08 13:44:48 -0300

    botão da tela de geração refeito

--- src/components/Geradores/BtnGera.tsx
+++ src/components/Geradores/BtnGera.tsx
@@ -1,15 +1,28 @@
 function BtnGera( props: { funcao: Function } ) {
 
+    /*
+        py-1
+        px-2
+        cursor-pointer
+        text-white
+        font-bold
+        bg-red-500
+        duration-300
+        ease-in
+        hover:rounded-full
+    */
+
     return <button className="
+                            px-3
                             py-1
-                            px-2
+                            text-xl
+                            opacity-75
+                            rounded-xl
                             cursor-pointer
-                            text-white
-                            font-bold
-                            bg-red-500
-                            duration-300
-                            ease-in
-                            hover:rounded-full"
+                            bg-sky-400
+                            hover:opacity-100
+                            hover:bg-sky-500
+                            hover:text-white"
                             onClick={ () => props.funcao()}>
                 Gerar
             </button>
