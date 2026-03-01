3f1ed6766acb1933aece5b47af817e7316307fd7
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-03-01 02:47:51 -0300

    feat: estrutura da lógica para buscar os cards melhoradac

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,32 @@ function App() {
   const [cards, setCards] = useState<CardData[]>([]);
 
   useEffect(() => { // carrega os cards do JSON
-    fetch('./cards.json', {
-      headers: {
-        Accept: "Application/json"
+    const carregarCards = async () => { // função para receber os cards
+      try {
+        const response = await fetch('/cards.json', {
+          headers: { // define que aceitará um json como resposta
+            Accept: "application/json"
+          }
+        });
+
+        // caso a requisição falhe
+        if(!response.ok) throw new Error(`Erro HTTP: ${response.status}`);
+        
+        //responsta transformada em JSON
+        const resJson = await response.json();
+
+        // validação..
+        if(resJson && Array.isArray(resJson.data)){
+          setCards(resJson.data);
+        } else {
+          throw new Error("Formato de JSON inválido: 'data' não é um array")
+        }
+      } catch(erro){
+        console.error("Falha ao carregar cards: ", erro);
       }
-    }).then( res => res.json() )
-      .then( res => setCards(res.data));
+    }
+
+    carregarCards(); // executa a função
   }, []);
 
   return (
