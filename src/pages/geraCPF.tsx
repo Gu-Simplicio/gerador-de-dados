48112eaaf9d21b3799a02fc222a0ff3bb203511b
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-03-15 22:55:25 -0300

    refactor(geraCPF): função de geração de CPF inserida em um service

--- src/pages/geraCPF.tsx
+++ src/pages/geraCPF.tsx
@@ -3,67 +3,14 @@ import Header from "../components/Header/Header";
 import BtnGera from "../components/Geradores/BtnGera";
 import InputGera from "../components/Geradores/InputGera";
 import TituloPag from "../components/Titulo/TituloPag";
+import gerarCPF from "../services/gerarCPF";
 
 function GeraCPF() {
     // CPF que será alterado conforme o usuário clica no botão..
     const [cpfGerado, setCpfGerado] = useState<string>('');
 
-    // cria um CPF contendo números aleatórios..
-    const gerarCPF = (): void => {
-        // letras do CPF..
-        let cpf = "";
-        // números do CPF, para gerar os 2 últimos números
-        let numsCpf: Array<number> = new Array<number>();
-
-        // gera o CPF com números aleatórios..
-        let auxLoop = 0;
-        do {
-            if(auxLoop == 3 || auxLoop == 7) { // pontos da frase do CPF
-                cpf += ".";
-            } else if(auxLoop == 11) { // traço do CPF
-                cpf += "-"
-            } else { // números do CPF
-                // gera um número aleatório de 0 à 9
-                const numAleatorio = Math.floor(Math.random() * 10);
-                // insere o número aleatório no cpf
-                cpf += numAleatorio;
-                // adiciona o número nos números do CPF
-                numsCpf.push(numAleatorio);
-            }
-
-            auxLoop++;
-        }while(auxLoop <= 11);
-
-        // CÁLCULO DO PRIMEIRO DÍGITO VERIFICADOR..
-        // pesos necessários para calcular
-        let pesos: Array<number> = new Array(10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
-        let produtoNums = 0; // produto usado para o cálculo
-        
-        for(let num in numsCpf) produtoNums += numsCpf[num] * pesos[num];
-        // recebe o resto da divisão, para gerar o primeiro número
-        let resto = produtoNums % 11;
-
-        // insere o número com base no resultado do resto
-        if(resto < 2) {
-            cpf += 0;
-            numsCpf.push(0);
-        } else {
-            cpf += 11 - resto;
-            numsCpf.push(11 - resto);
-        }
-
-        // CÁLCULO DO SEGUNDO DÍGITO VERIFICADOR..
-        pesos.unshift(11); // adiciona um peso novo
-        produtoNums = 0;
-        for(let num in numsCpf) produtoNums += numsCpf[num] * pesos[num];
-
-        // novamente, o resto da divisão por 11..
-        resto = produtoNums % 11;
-        // insere conforme o resultado do resto
-        resto < 2 ? cpf += 0 : cpf += 11 - resto;
-
-        setCpfGerado(cpf); //altera o CPF
-    }
+    // chama a função que gera um novo CPF de services
+    const novoCPF = (): void => setCpfGerado(gerarCPF());
 
     return (
         <>
@@ -81,7 +28,7 @@ function GeraCPF() {
                 <div className="
                     w-full
                     flex justify-center gap-10">
-                    <BtnGera funcao={gerarCPF}/>
+                    <BtnGera funcao={novoCPF}/>
 
                     <InputGera
                         id="inCPF"
---
+++ src/services/gerarCPF.ts
@@ -0,0 +1,56 @@
+// cria um CPF contendo números aleatórios..
+export default function gerarCPF(): string {
+    // letras do CPF..
+    let cpf = "";
+    // números do CPF, para gerar os 2 últimos números
+    let numsCpf: Array<number> = new Array<number>();
+
+    // gera o CPF com números aleatórios..
+    let auxLoop = 0;
+    do {
+        if(auxLoop == 3 || auxLoop == 7) { // pontos da frase do CPF
+            cpf += ".";
+        } else if(auxLoop == 11) { // traço do CPF
+            cpf += "-"
+        } else { // números do CPF
+            // gera um número aleatório de 0 à 9
+            const numAleatorio = Math.floor(Math.random() * 10);
+            // insere o número aleatório no cpf
+            cpf += numAleatorio;
+            // adiciona o número nos números do CPF
+            numsCpf.push(numAleatorio);
+        }
+
+        auxLoop++;
+    }while(auxLoop <= 11);
+
+    // CÁLCULO DO PRIMEIRO DÍGITO VERIFICADOR..
+    // pesos necessários para calcular
+    let pesos: Array<number> = new Array(10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+    let produtoNums = 0; // produto usado para o cálculo
+    
+    for(let num in numsCpf) produtoNums += numsCpf[num] * pesos[num];
+    // recebe o resto da divisão, para gerar o primeiro número
+    let resto = produtoNums % 11;
+
+    // insere o número com base no resultado do resto
+    if(resto < 2) {
+        cpf += 0;
+        numsCpf.push(0);
+    } else {
+        cpf += 11 - resto;
+        numsCpf.push(11 - resto);
+    }
+
+    // CÁLCULO DO SEGUNDO DÍGITO VERIFICADOR..
+    pesos.unshift(11); // adiciona um peso novo
+    produtoNums = 0;
+    for(let num in numsCpf) produtoNums += numsCpf[num] * pesos[num];
+
+    // novamente, o resto da divisão por 11..
+    resto = produtoNums % 11;
+    // insere conforme o resultado do resto
+    resto < 2 ? cpf += 0 : cpf += 11 - resto;
+
+    return cpf;
+}
