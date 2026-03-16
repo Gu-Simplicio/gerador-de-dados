e7c02d47fba2ad617945f6c89465ed223b1ebdbe
Gu-Simplicio <gustavosilvasimplicio08@gmail.com>
2026-03-16 14:46:09 -0300

    refactor(getCards): lógica de receber os cards tranferida para um service

---
+++ src/services/appService.ts
@@ -0,0 +1,24 @@
+export default async function getCards(): Promise<any> {
+    try {
+        const response = await fetch('/cards.json', {
+            headers: { // define que aceitará um json como resposta
+            Accept: "application/json"
+            }
+        });
+
+        // caso a requisição falhe
+        if(!response.ok) throw new Error(`Erro HTTP: ${response.status}`);
+    
+        //responsta transformada em JSON
+        const resJson = await response.json();
+
+        // validação..
+        if(resJson && Array.isArray(resJson.data)){
+            return resJson.data;
+        } else {
+            throw new Error("Formato de JSON inválido: 'data' não é um array")
+        }
+    } catch(erro){
+        console.error("Falha ao carregar cards: ", erro);
+    }
+}
